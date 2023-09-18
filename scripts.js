87a226958663b555261a0207d83cfbee9fcf57f3
Minh Frances <131257637+htmlcssjvscrpt@users.noreply.github.com>
2023-09-18 08:24:11 +0800

    Update file

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 function validatePasswords() {
     let passwordElement = document.getElementById('password');
     let confirmPasswordElement = document.getElementById('confirm_password'); 
+    let passwordError = document.getElementById('passwordError');
 
     let passwordValue = passwordElement.value;
     let confirmPasswordValue = confirmPasswordElement.value;
@@ -8,9 +9,13 @@ function validatePasswords() {
     if(passwordValue !== confirmPasswordValue) {
         passwordElement.style.borderColor = 'red';
         confirmPasswordElement.style.borderColor = 'red';
-        alert('Passwords do not match. Try again');
-        
+        passwordError.textContent = 'Passwords do not match. Try again';
         return false;
+    }  else {
+        // If passwords match, clear any error messages and reset border colors
+        passwordError.textContent = '';
+        passwordElement.style.borderColor = '';  // This will reset to CSS styles or you can define a specific color
+        confirmPasswordElement.style.borderColor = ''; // This will reset to CSS styles or you can define a specific color
     }
 
     return true;
